708bfff4adee9736841303fc3105f4a1228e9b2c
dapada-hash <darlyjeanstreaming1@gmail.com>
2026-05-17 17:15:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ PERIOD_OPTIONS = [
 QUIZ_QUESTIONS = [
     {
         "id": 1,
-        "question": "Which of the following checks for value equality between two lists A = [1, 2, 3, 4], and \n\tB = ['a', 'b', 'c', 'd']?",
+        "question": "Which of the following checks for value equality between two lists <br>&nbsp;&nbsp;&nbsp;&nbsp;A = [1, 2, 3, 4], <br>&nbsp;&nbsp;&nbsp;&nbsp;B = ['a', 'b', 'c', 'd']?",
         "type": "mc",
         "options": ["A = B", "A == B", "A is B", "A != B"],
         "answer": "A == B",
